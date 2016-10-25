bc7f09d6e71363c33ad6b77cd4043fbad69b8f51
Devin Ludwig and Erica Nafziger <devin@>
2016-10-25 14:57:43 -0700

    add simpler javascript to return result, also add functionality if no number is entered

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,38 @@
 var number = prompt("Enter a number:");
-
-var array = []
-
-if(number < 0) {
-  alert(undefined)
-  location.reload();
+var total = 1;
+if (number) {
+  if (number < 0) {
+    alert(undefined);
+    location.reload();
+  }  else if (number >= 0) {
+    for(var index = number; index >= 1; index -= 1) {
+      total *= index;
+    }
+    alert(total);
+  } 
 } else {
-
-for(var index = number; index >= 1; index -= 1) {
-  array.push(index);
+  alert("you didn't enter a number!");
+  location.reload();
 }
 
-var total = 1
 
-array.forEach(function(inputNumber) {
-  total *= inputNumber;
-});
 
-alert(total);
-}
+// var array = []
+//
+// if(number < 0) {
+//   alert(undefined)
+//   location.reload();
+// } else {
+//
+// for(var index = number; index >= 1; index -= 1) {
+//   array.push(index);
+// }
+//
+//
+// var total = 1
+//
+// array.forEach(function(inputNumber) {
+//   total *= inputNumber;
+// });
+
+// alert(total);
